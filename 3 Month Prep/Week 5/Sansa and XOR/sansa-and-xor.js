8b3ad695db9662dd8a568110298c9f18d8ae962c
Francois Vandette <100052703+francoisvandette@users.noreply.github.com>
2022-07-10 17:10:56 -0700

    Sansa and XOR

--- 3 Month Prep/Week 5/Sansa and XOR/sansa-and-xor.js
+++ 3 Month Prep/Week 5/Sansa and XOR/sansa-and-xor.js
@@ -1,5 +1,6 @@
 function sansaXor(arr) {
-    // Write your code here
+
+    // the answer discovered in the discussion for this problem
     // let result = 0;
     // if (arr.length % 2 == 0) {
     //     return 0;
@@ -10,17 +11,18 @@ function sansaXor(arr) {
     // }
     // return result;
     
+    // my brute force method that uses
     let ray = [];
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j+i+1 <= arr.length; j++) {
             ray.push(xorArray(arr.slice(j,j+i+1)));
         }
     }
-    // console.log(ray);
     
     return xorArray(ray);
 }
 
+// method I wrote to assist in xor-ing an array of numbers
 function xorArray(arr) {
     let result = 0;
     for (let i = 0; i < arr.length; i++) {
